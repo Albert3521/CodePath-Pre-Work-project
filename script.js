d8b84c9ca83b73e47767ca809095d71de0dfe0ac
Glitch (glitch-hello-website) <none>
2022-03-29 22:18:50 +0000

    👋🏗 Checkpoint ./script.js:43013998/478 ./index.html:43013998/49

--- script.js
+++ script.js
@@ -3,3 +3,21 @@ var pattern = [2,4,2,3,1,1,4,3]
 var progress = 0;
 var gamePlaying = false;
 
+function startGame()
+{
+  //Initialize Game Variables
+  progress = 0;
+  gamePlaying = true;
+  
+  //swapping start and sto buttons
+  document.getElementById("startBtn").classList.add("hideen");
+  document.getElementById("stopBtn").classList.remove("hidden");
+  
+}
+
+function stopGame()
+{
+  gamePlaying = false;
+  document.getElementById("startBtn").classList.remove("hidden");
+  document.getElementById("stopBtn").classList.add("hidden");
+}
